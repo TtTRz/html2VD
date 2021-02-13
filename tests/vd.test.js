2133ab07ec8ec9eda54f750757570f431ed108ee
RomChung <thelns@vip.qq.com>
2021-02-13 14:59:20 +0800

    add: test cases

--- tests/vd.test.js
+++ tests/vd.test.js
@@ -2,8 +2,9 @@ const { TestScheduler } = require('jest')
 const {html_2_vd} = require('./pkg/html2VD.js')
 const VDD = require('./utils/vdDiff.js');
 
-describe("vd", () => {
-  const html = `
+describe("parse simple html", () => {
+  test("single tag", () => {
+    const html = `
     <div>
       Rom
     </div>
@@ -31,7 +32,227 @@ describe("vd", () => {
       }
     ]
   }
-  test("test parse html", () => {
+    let vdd = new VDD();
+    let vdObj = html_2_vd(html)
+    expect(vdObj).toEqual(targetObj)
+    vdd.diff_vd(vdObj, targetObj);
+    let diff_result = vdd.diff_result;
+    expect(diff_result).toBe(true)
+  })
+
+  test("multiple tag", () => {
+    const html = `
+      <div>
+        Rom
+      </div>
+      <span>
+        Chung
+      </span>
+    `
+    const targetObj = {
+      tag: null,
+      inner_html: null,
+      attrs: [],
+      node_type: 'FragmentNode',
+      children: [
+        {
+          tag: 'div',
+          inner_html: null,
+          attrs: [],
+          node_type: 'ElementNode',
+          children: [
+            {
+              tag: null,
+              inner_html: "Rom",
+              attrs: [],
+              node_type: 'TextNode',
+              children: []
+            }
+          ]
+        },
+        {
+          tag: 'span',
+          inner_html: null,
+          attrs: [],
+          node_type: 'ElementNode',
+          children: [
+            {
+              tag: null,
+              inner_html: "Chung",
+              attrs: [],
+              node_type: 'TextNode',
+              children: []
+            }
+          ]
+        }
+      ]
+    }
+    let vdd = new VDD();
+    let vdObj = html_2_vd(html)
+    expect(vdObj).toEqual(targetObj)
+    vdd.diff_vd(vdObj, targetObj);
+    let diff_result = vdd.diff_result;
+    expect(diff_result).toBe(true)
+  })
+})
+
+describe("parse simple html with attrs", () => {
+  test("single tag with attrs", () => {
+    const html = `
+    <div class="title" id="name">
+      Rom
+    </div>
+  `
+  const targetObj = {
+    tag: null,
+    inner_html: null,
+    attrs: [],
+    node_type: 'FragmentNode',
+    children: [
+      {
+        tag: 'div',
+        inner_html: null,
+        attrs: [{
+          name: "class",
+          value: "title"
+        }, {
+          name: "id",
+          value: "name"
+        }],
+        node_type: 'ElementNode',
+        children: [
+          {
+            tag: null,
+            inner_html: "Rom",
+            attrs: [],
+            node_type: 'TextNode',
+            children: []
+          }
+        ]
+      }
+    ]
+  }
+    let vdd = new VDD();
+    let vdObj = html_2_vd(html)
+    expect(vdObj).toEqual(targetObj)
+    vdd.diff_vd(vdObj, targetObj);
+    let diff_result = vdd.diff_result;
+    expect(diff_result).toBe(true)
+  })
+
+  test("multiple tag with attrs", () => {
+    const html = `
+      <div class="title" style="width: 100px">
+        Rom
+      </div>
+      <span id="rom">
+        Chung
+      </span>
+    `
+    const targetObj = {
+      tag: null,
+      inner_html: null,
+      attrs: [],
+      node_type: 'FragmentNode',
+      children: [
+        {
+          tag: 'div',
+          inner_html: null,
+          attrs: [{
+            name: "class",
+            value: "title"
+          }, {
+            name: "style",
+            value: "width: 100px",
+          }],
+          node_type: 'ElementNode',
+          children: [
+            {
+              tag: null,
+              inner_html: "Rom",
+              attrs: [],
+              node_type: 'TextNode',
+              children: []
+            }
+          ]
+        },
+        {
+          tag: 'span',
+          inner_html: null,
+          attrs: [{
+            name: "id",
+            value: "rom"
+          }],
+          node_type: 'ElementNode',
+          children: [
+            {
+              tag: null,
+              inner_html: "Chung",
+              attrs: [],
+              node_type: 'TextNode',
+              children: []
+            }
+          ]
+        }
+      ]
+    }
+    let vdd = new VDD();
+    let vdObj = html_2_vd(html)
+    expect(vdObj).toEqual(targetObj)
+    vdd.diff_vd(vdObj, targetObj);
+    let diff_result = vdd.diff_result;
+    expect(diff_result).toBe(true)
+  })
+})
+
+describe("parse complex html", () => {
+  test("single tag", () => {
+    const html = `
+      <div>
+        Rom
+        <span>
+          Chung
+        </span>
+      </div>
+    `
+    const targetObj = {
+      tag: null,
+      inner_html: null,
+      attrs: [],
+      node_type: 'FragmentNode',
+      children: [
+        {
+          tag: 'div',
+          inner_html: null,
+          attrs: [],
+          node_type: 'ElementNode',
+          children: [
+            {
+              tag: null,
+              inner_html: "Rom",
+              attrs: [],
+              node_type: 'TextNode',
+              children: []
+            },
+            {
+              tag: 'span',
+              inner_html: null,
+              attrs: [],
+              node_type: 'ElementNode',
+              children: [
+                {
+                  tag: null,
+                  inner_html: "Chung",
+                  attrs: [],
+                  node_type: 'TextNode',
+                  children: []
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
     let vdd = new VDD();
     let vdObj = html_2_vd(html)
     expect(vdObj).toEqual(targetObj)
